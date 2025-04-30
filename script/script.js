ac84ae6fa07468bceda746b49f75a528cafa588b
Ivone Gutierrez <liguro.101212@gmail.com>
2025-04-29 22:28:56 -0500

    edicion del dia 29 de abril, solo hice nav

--- script/script.js
+++ script/script.js
@@ -1,19 +1,2 @@
-console.log ("Hola, Soy Ivone Gutierrez");
-//VARIABLES
-// automatica
-// recomendado usar nombre descriptivos
-nombreVariable = "Ivone"
 
-console.log (nombreVariable)
-
-//OPERADORES
-//asignacion
-
-//TIPOS DE DATO PRIMITIVOS
-//string, cadena de texto
-
-//DATO O VALOR
-
-let miVariable = "texto"
-console.log ("miVariable:", miVariable)
 
